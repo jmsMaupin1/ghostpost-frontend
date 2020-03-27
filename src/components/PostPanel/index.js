de5c509002c76776a11b49ddeb9ea6c863ba0082
James Maupin <jmsMaupinRC@gmail.com>
2020-03-26 23:19:42 -0400

    refactor list of posts into a component

---
+++ src/components/PostPanel/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+
+import Post from '../Post'
+
+function send_upvote(id, setPosts) {
+  fetch(`http://localhost:8000/api/posts/${id}/upvote`)
+  .then(res => res.json())
+  .then(result => {
+    getPosts(setPosts)
+  })
+}
+
+function send_downvote(id, setPosts) {
+  fetch(`http://localhost:8000/api/posts/${id}/downvote`)
+  .then(res => res.json())
+  .then(result => {
+    getPosts(setPosts)
+  })
+}
+
+function getPosts(setPosts) {
+    fetch("http://localhost:8000/api/posts/")
+    .then(res => res.json())
+    .then(json => {
+      setPosts(json)
+    })
+}
+
+export default function PostPanel() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        getPosts(setPosts)
+    }, [])
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            {
+              posts.map((cur, index) => {
+                return <Post 
+                  content={cur.content}
+                  upvotes={cur.upvotes}
+                  downvotes={cur.downvotes}
+                  isBoast={cur.isBoast}
+                  datetime={cur.datetime}
+                  key={cur.id}
+                  upvote={send_upvote.bind(this, cur.id, setPosts)}
+                  downvote={send_downvote.bind(this, cur.id, setPosts)}
+                />
+              })
+            }
+          </header>
+        </div>
+      );
+}
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,57 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+import PostPanel from '../../components/PostPanel';
 import './App.css';
 
-import Post from '../../components/post'
-
-function send_upvote(id, setPosts) {
-  fetch(`http://localhost:8000/api/posts/${id}/upvote`)
-  .then(res => res.json())
-  .then(result => {
-    getPosts(setPosts)
-  })
-}
-
-function send_downvote(id, setPosts) {
-  fetch(`http://localhost:8000/api/posts/${id}/downvote`)
-  .then(res => res.json())
-  .then(result => {
-    getPosts(setPosts)
-  })
-}
-
-function getPosts(setPosts) {
-    fetch("http://localhost:8000/api/posts/")
-    .then(res => res.json())
-    .then(json => {
-      setPosts(json)
-    })
-}
-
 function App() {
-
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    getPosts(setPosts)
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
-        {
-          posts.map((cur, index) => {
-            return <Post 
-              content={cur.content}
-              upvotes={cur.upvotes}
-              downvotes={cur.downvotes}
-              isBoast={cur.isBoast}
-              datetime={cur.datetime}
-              key={cur.id}
-              upvote={send_upvote.bind(this, cur.id, setPosts)}
-              downvote={send_downvote.bind(this, cur.id, setPosts)}
-            />
-          })
-        }
+        <PostPanel />
       </header>
     </div>
   );
